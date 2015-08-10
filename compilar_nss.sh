667da3ea2fa291c55f278b7dd7bfdfb63dd632fe
Axel Díaz <diaz.axelio@gmail.com>
2015-08-10 12:40:23 -0430

    Mejorando script con dependencia a canaima-certificados

--- compilar_nss.sh
+++ compilar_nss.sh
@@ -4,35 +4,29 @@ EXITO() {
 printf "\033[1;32m${1}\033[0m\n"
 }
  
-mkdir -p ~/public_html/certificados_iceweasel/certificados/
-mkdir -p ~/public_html/certificados_iceweasel/paquetes/
-cd ~/public_html/certificados_iceweasel/
+PATH_CERTS='/tmp/certificados'
+PATH_MOZILLA='~/public_html/certificados_iceweasel'
+
+mkdir -p $PATH_MOZILLA/certificados/
+mkdir -p $PATH_MOZILLA/paquetes/
+mkdir -p $PATH_CERTS
+cd $PATH_MOZILLA
  
 EXITO "Instalando dependencias necesarias"
-su -c "aptitude update; aptitude install git git-buildpackage build-essential quilt libc6-dev zlib1g-dev libnspr4-dev libsqlite3-dev"
- 
-echo " "
-EXITO "Iniciando procedimiento para obtencion del certificado"
-git clone https://gist.github.com/Axelio/0a86cddf72dadfd43426 get_certificados
- 
-echo " "
-EXITO "Descargando los siguientes certificados:"
-cd certificados/
-python ~/public_html/certificados_iceweasel/get_certificados/descargar_certificados.py
+su -c "aptitude update; aptitude install git git-buildpackage build-essential quilt libc6-dev zlib1g-dev libnspr4-dev libsqlite3-dev canaima-certificados"
  
 echo " "
 EXITO "Obteniendo código fuente de nss y nspr"
-cd ~/public_html/certificados_iceweasel/paquetes/
-rm -rf ~/public_html/certificados_iceweasel/get_certificados/
+cd $PATH_MOZILLA/paquetes/
 apt-get source nss nspr
  
 echo " "
 EXITO "Quitando patchs"
 VERSION_NSS=`ls -d */ | grep nss | cut -d \- -f 2 | cut -d \/ -f 1`
 VERSION_NSPR=`ls -d */ | grep nspr | cut -d \- -f 2 | cut -d \/ -f 1`
-VERSION_NSS_CHANGELOG=`head -1 ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/debian/changelog | tail -1 | cut -d \( -f 2 | cut -d \) -f 1`
+VERSION_NSS_CHANGELOG=`head -1 ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/debian/changelog | tail -1 | cut -d \( -f 2 | cut -d \) -f 1` 
  
-cd ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/
+cd $PATH_MOZILLA/paquetes/nss-$VERSION_NSS/
 export QUILT_PATCHES=debian/patches
 quilt pop -a
 rm -rf .pc
@@ -42,55 +36,59 @@ git init
 git add .
 git commit -a -m "Versión original del código fuente."
  
-cd ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/mozilla/
-ln -s ~/public_html/certificados_iceweasel/paquetes/nspr-$VERSION_NSPR/mozilla/nsprpub/ .
+cd $PATH_MOZILLA/paquetes/nss-$VERSION_NSS/mozilla/
+ln -s $PATH_MOZILLA/paquetes/nspr-$VERSION_NSPR/mozilla/nsprpub/ .
  
 echo $LIST
  
 su -c '
-if echo "$COMMAND" | grep -q "$SOURCE"; then
-cd ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/mozilla/security/nss/
-make nss_build_all BUILD_OPT=1 USE_64=1
- 
-cd ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/mozilla/security/nss/cmd/addbuiltin/
-make BUILD_OPT=1 USE_64=1
- 
-echo " "
-echo "Se copiará Linux3.2_x86_64_glibc_PTH_64_OPT.OBJ a /usr/bin/"
-echo "Se debe ejecutar como super usuario"
-cp -v Linux3.2_x86_64_glibc_PTH_64_OPT.OBJ/addbuiltin /usr/bin/"
-else
-cd ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/mozilla/security/nss/
-make nss_build_all BUILD_OPT=1
- 
-cd ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/mozilla/security/nss/cmd/addbuiltin/
-make BUILD_OPT=1
- 
-echo " "
-echo "Se copiará Linux3.2_x86_glibc_PTH_OPT.OBJ a /usr/bin/"
-echo "Se debe ejecutar como super usuario"
-su -c "cp -v Linux3.2_x86_glibc_PTH_OPT.OBJ/addbuiltin /usr/bin/"
-fi
+    if echo "$COMMAND" | grep -q "$SOURCE"; then
+        cd $PATH_MOZILLA/paquetes/nss-$VERSION_NSS/mozilla/security/nss/
+        make nss_build_all BUILD_OPT=1 USE_64=1
+         
+        cd $PATH_MOZILLA/paquetes/nss-$VERSION_NSS/mozilla/security/nss/cmd/addbuiltin/
+        make BUILD_OPT=1 USE_64=1
+         
+        echo " "
+        echo "Se copiará Linux3.2_x86_64_glibc_PTH_64_OPT.OBJ a /usr/bin/"
+        echo "Se debe ejecutar como super usuario"
+        cp -v Linux3.2_x86_64_glibc_PTH_64_OPT.OBJ/addbuiltin /usr/bin/"
+    else
+        cd $PATH_MOZILLA/paquetes/nss-$VERSION_NSS/mozilla/security/nss/
+        make nss_build_all BUILD_OPT=1
+         
+        cd $PATH_MOZILLA/paquetes/nss-$VERSION_NSS/mozilla/security/nss/cmd/addbuiltin/
+        make BUILD_OPT=1
+         
+        echo " "
+        echo "Se copiará Linux3.2_x86_glibc_PTH_OPT.OBJ a /usr/bin/"
+        echo "Se debe ejecutar como super usuario"
+        su -c "cp -v Linux3.2_x86_glibc_PTH_OPT.OBJ/addbuiltin /usr/bin/"
+    fi
 '
  
 echo " "
 EXITO "Convirtiendo los certificados con addbuiltin"
-cd ~/public_html/certificados_iceweasel/certificados/
+cp /etc/ssl/certs/* $PATH_CERTS
  
-CERTIFICADOS=`ls *.crt`
+CERTIFICADOS=`ls $PATH_CERTS`
  
 for certificado in $CERTIFICADOS
-do
-nombre=`echo $certificado | cut -d \. -f 1`
-openssl x509 -inform PEM -outform DER -in $certificado -out $nombre.der
-comando=`openssl x509 -inform PEM -text -in $certificado | grep "Subject"`
-O=`echo $comando | cut -d \O -f 2 | cut -d \= -f 2 | cut -d \, -f 1`
-comando=`cat $nombre.der | addbuiltin -n "$O" -t "C,C,C" > $nombre.nss`
-done
+    do
+        if [ `echo $certificado | cut -d \. -f 2;` != 0 ]; then
+            nombre=`echo $certificado | cut -d \. -f 1`
+        else
+            nombre=$certificado
+        fi
+        openssl x509 -inform PEM -outform DER -in $certificado -out $nombre.der
+        comando=`openssl x509 -inform PEM -text -in $certificado | grep "Subject"`
+        O=`echo $comando | cut -d \O -f 2 | cut -d \= -f 2 | cut -d \, -f 1`
+        comando=`cat $nombre.der | addbuiltin -n "$O" -t "C,C,C" > $nombre.nss`
+    done
  
 echo " "
 echo "Parcheando y reempaquetando NSS"
-cd ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/
+cd $PATH_MOZILLA/paquetes/nss-$VERSION_NSS/
 git reset --hard
 git clean -fd
  
@@ -99,20 +97,20 @@ version=`echo $version | cut -d \: -f 2`
  
 git tag debian/$version
  
-cd ~/public_html/certificados_iceweasel/certificados/
+cd $PATH_MOZILLA/certificados/
  
 CERTIFICADOS=`ls *.nss`
  
 for certificado in $CERTIFICADOS
-do
-cat $certificado >> ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/mozilla/security/nss/lib/ckfw/builtins/certdata.txt
-done
+    do
+        cat $certificado >> $PATH_MOZILLA/paquetes/nss-$VERSION_NSS/mozilla/security/nss/lib/ckfw/builtins/certdata.txt
+    done
  
-cd ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/mozilla/security/nss/lib/ckfw/builtins/
+cd $PATH_MOZILLA/paquetes/nss-$VERSION_NSS/mozilla/security/nss/lib/ckfw/builtins/
 make generate
  
  
-cd ~/public_html/certificados_iceweasel/paquetes/nss-$VERSION_NSS/
+cd PATH_MOZILLA/paquetes/nss-$VERSION_NSS/
 mkdir -p debian/patches
  
 echo " "
@@ -137,5 +135,5 @@ EXITO "Subiendo de versión el paquete NSS"
 git dch -N $VERSION_NSS_CHANGELOG+1canaima --release --auto --id-length=7 --full --git-author
  
 git add .
-git commit -am "Agregando parche para los certificados aprobados por SUSCERTE"
+git commit -am "Agregando parche para los certificados de los Proveedores de Servicios de Certificación por SUSCERTE"
 git-buildpackage -us -uc -j5
